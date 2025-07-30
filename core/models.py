71c7a6359a9817168eeaa0a9a444aef92669bbce
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-07-30 22:51:09 +0600

    first commit

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Resturant(models.Model):
+    name = models.CharField(max_length=100)
+    website = models.URLField(default='')
+    date_opened = models.DateField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+    
+
+    
