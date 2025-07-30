29156173362996c72a44180ceec6e0e51308bdb0
Shahriar Azam Khan <shahriarazamkhan@gmail.com>
2025-07-30 23:13:23 +0600

    modelsCreated

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from core.models import Resturant, Sale, Rating
 
-# Register your models here.
+admin.site.register(Resturant)
+admin.site.register(Sale)
+admin.site.register(Rating)
--- core/models.py
+++ core/models.py
@@ -1,14 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 class Resturant(models.Model):
+
+    class TypeChoices(models.TextChoices):
+        INDIAN = 'IN', 'Indian'
+        BANGLADESHI = 'BN', 'Bangladeshi'
+        CHINESE = 'CH', 'Chinese'
+        ITALIAN = 'IT', 'Italian'
+        GREEK = 'GR', 'Greek'
+        FASTFOOD = 'FF', 'Fast Food'
+        MEXICAN = 'MX', 'Mexican'
+        OTHER = 'OT', 'Other'
+
+
     name = models.CharField(max_length=100)
     website = models.URLField(default='')
     date_opened = models.DateField()
     latitude = models.FloatField()
     longitude = models.FloatField()
+    resturant_type = models.CharField(max_length=2, choices=TypeChoices.choices, default='' )
 
     def __str__(self):
         return self.name
     
 
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    resturant = models.ForeignKey(Resturant, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"Rating : {self.rating}"
     
+
+class Sale(models.Model):
+    resturant = models.ForeignKey(Resturant, on_delete=models.SET_NULL, null=True)
+    income = models.DecimalField(max_digits=8, decimal_places=2)
+    datetime = models.DateTimeField()
